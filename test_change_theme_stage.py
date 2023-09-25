8eeb825299da85da9c0754e403d9e838e6b52ce8
edvibecompany <edvibe.company@gmail.com>
2023-09-25 10:04:42 +0300

    test for changing theme on stage

---
+++ test_change_theme_stage.py
@@ -0,0 +1,18 @@
+from selene import browser, have, be
+
+
+def test_change_account_theme(login):
+    browser.wait_until(browser.element('.edvibe3-shedule').should(be.visible))
+
+    # Смена темы на темную
+    browser.element('.user-popover .iconedv-Arrow-More-Down').click()
+    browser.element('.theme-block_button:nth-child(3)').click()
+    browser.element('.theme-block_button:nth-child(3)').should(have.css_class('activeTheme'))
+
+    # Смена темы на светлую
+    browser.element('.theme-block_button:nth-child(5)').click()
+    browser.element('.theme-block_button:nth-child(5)').should(have.css_class('activeTheme'))
+
+    # Смена темы на системную
+    browser.element('.theme-block_button:nth-child(1)').click()
+    browser.element('.theme-block_button:nth-child(1)').should(have.css_class('activeTheme'))
